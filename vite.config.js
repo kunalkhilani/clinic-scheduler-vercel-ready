fdd929e2a497fa64b0e6cd2679be44e251a72587
kunalkhilani <89785514+kunalkhilani@users.noreply.github.com>
2025-04-23 18:21:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
-  root: '.',
-  server: {
-    port: 3000
-  },
+  build: {
+    outDir: '../dist'
+  }
 });
